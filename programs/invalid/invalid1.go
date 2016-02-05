606e484ad1cc24a906c44f3584bf8c074d1e3a7a
Cheuk Chuen Siow <cheukchuen@gmail.com>
2016-02-05 18:32:19 -0500

    Added test programs and tex file

---
+++ programs/invalid/invalid1.go
@@ -0,0 +1,11 @@
+/* Block comments do not nest
+   /* The star-slash on the right ends the above comment */
+   This is an error */
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, world!")
+}
---
+++ programs/valid/valid1.go
@@ -0,0 +1,12 @@
+// This is a line comment
+
+/* This is a
+   block comment */
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, world!")
+}
