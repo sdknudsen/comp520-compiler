b945ec9c0387f094b7a998e0124cba041404038b
Stefan Knudsen <stefan.knudsen@mail.mcgill.ca>
2016-02-13 15:43:08 -0500

    changes to context module

--- src/context.ml
+++ src/context.ml
@@ -1,4 +1,4 @@
-exception EmptyContext
+exception ContextError of string
 (* use a map instead of a hash table? *)
 (* module Ctx = Map.Make(String) *)
 (* I'm not sure how correct everything is *)
@@ -9,30 +9,27 @@ module Ctx =
         type values = { scp : string; typ : string };;
         type context = (values Frame.t) list
 
-        let create () = [Frame.empty]
+        let create () = [Frame.empty] (* can we do this without unit? *)
 
         let scope stack = Frame.empty::stack
 
-        let unscope = function
-          | hd::tl -> tl
-          | _ -> raise EmptyContext
+        let unscope stack = try List.tail stack
+                            with _ -> raise ContextError "Empty Context"
 
-        let find stack = Frame.empty::stack
+        let find g = Frame.empty::g
 
-        let is_empty = function
-          | [x] -> Frame.is_empty x
-          | x::xs -> false
-          | [] -> raise EmptyContext
+        let is_empty g = List.for_all (fun x -> Frame.is_empty x) g
 
-        let rec mem k g = List.exists (fun x -> Frame.mem k x) g
+        let mem k g = List.exists (fun x -> Frame.mem k x) g
 
-        let add k v g = match g with
-          | x::xs -> Frame.add 
-          | [] -> raise EmptyContext
+        let add k v g = try Frame.add k v (List.head g)
+                        with _ -> raise ContextError "Empty Context"
 
         let find k v g = match g with
-          | x::xs -> Frame.find  
-          | [] -> raise EmptyContext
+          | x::xs -> if Frame.mem k x
+                     then Frame.find k x
+                     else find k v xs
+          | [] -> raise ContextError "Undeclared Variable"
 
         (* let remove (\* remove the first instance found? *\) *)
         (* let equal *)
