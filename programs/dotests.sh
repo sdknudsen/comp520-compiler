05e642cbbdc908dffb3f06ea218cc10dcc4ff4a6
astlouisf <astlouisf@gmail.com>
2016-02-23 08:55:42 -0500

    Fixes dotests.sh and adds other tests.

--- programs/dotests.sh
+++ programs/dotests.sh
@@ -5,29 +5,47 @@ GOC=../src/main.native
 
 echo "== Testing $1 ================================"
 
-for f in $(ls $1/*.valid $1/*.invalid); do
+case $1 in
+"lex"|"parse")
+files=$(ls $1/*.valid $1/*.invalid)
+
+;;
+"pretty")
+files=$(ls $1/*.go)
+
+;;
+esac
+
+for f in $files; do
   printf ' %s' $f
   printf '%*.*s' 0 $((40 - ${#f})) "$pad"
   fname=$(echo $f | cut -f 1 -d '.')
   ext="${f##*.}" 
 
   expected=0
-  if [ -f "$fname.expected" ]
+
+  if [ $1 = "pretty" ]
   then
-    ../src/main.native lex < $f | diff $fname.expected - > /dev/null 2>&1
+      ../src/main.native pretty < $f > "$fname.pretty"
+      ../src/main.native pretty "$fname.pretty" |  diff $fname.pretty - > /dev/null 2>&1
   else
-    if [ $ext = "invalid" ]
+    if [ -f "$fname.expected" ]
     then
-      expected=1
+      ../src/main.native $1 < $f | diff $fname.expected - > /dev/null 2>&1
+    else
+      if [ $ext = "invalid" ]
+      then
+        expected=1
+      fi
+      ../src/main.native $1 < $f > /dev/null 2>&1
     fi
-    ../src/main.native lex < $f > /dev/null 2>&1
   fi
 
   result=$?
-  if [ $result -eq $expected ]
+  if [ $result -eq $expected ] # 0 or 1
   then
     echo -e "[\e[32mpassed\e[39m]"
-  elif [ $? -eq $((1 - $expected)) ]
+  elif [ $result -eq $((1 - $expected)) ] # 0 or 1
   then
     echo -e "[\e[31mfailed\e[39m]"
   else
