cfd9af36cf10c3213661e6924cdf51157dcfd3c0
astlouisf <astlouisf@gmail.com>
2016-03-20 11:30:41 -0400

    ./dotests does not outputs valid passed tests

--- programs/dotests.sh
+++ programs/dotests.sh
@@ -3,6 +3,9 @@
 pad=$(printf '%0.1s' "."{1..80})
 GOC=../src/main.native
 
+passed=0
+total=0
+
 echo "== Testing $1 ================================"
 
 case $1 in
@@ -14,11 +17,13 @@ files=$(find $1 | egrep  "\.invalid$|\.valid$" | sort)
 files=$(ls $1/*.go $1/*.valid)
 
 ;;
+"weed")
+files=$(find "parse" | egrep  "\.invalid$|\.valid$" | sort)
+
+
 esac
 
 for f in $files; do
-  printf '%s ' $f
-  printf '%*.*s' 0 $((80 - ${#f} - 10)) "$pad"
   fname=$(echo $f | cut -f 1 -d '.')
   ext="${f##*.}" 
 
@@ -26,31 +31,38 @@ for f in $files; do
 
   if [ $1 = "pretty" ]
   then
-      $GOC pretty < $f > "$fname.pretty"
-      $GOC pretty "$fname.pretty" |  diff $fname.pretty - > /dev/null 2>&1
+      $GOC $1 < $f > "$fname.pretty"
+      $GOC $1 "$fname.pretty" |  diff $fname.pretty - > /dev/null 2>&1
   else
     if [ -f "$fname.expected" ]
     then
-      $GOC $1 < $f | diff $fname.expected - > /dev/null 2>&1
+      ({ error=$($GOC $1 < $f 2>&1 1>&$out); } {out}>&1) | diff $fname.expected - > /dev/null 
     else
       if [ $ext = "invalid" ]
       then
         expected=1
       fi
-      $GOC $1 < $f > /dev/null 2>&1
+      { error=$($GOC $1 < $f 2>&1 1>/dev/null); }
     fi
   fi
+
+  #$GOC $1 < $f > /dev/null 2>&1
 
   result=$?
   if [ $result -eq $expected ] # 0 or 1
   then
-    echo -e " [\e[32mpassed\e[39m]"
+    if [ $ext = "invalid" ]
+    then
+      printf '%*.*s' 0 70 "$f $pad"
+      echo -e " [\e[32mpassed\e[39m]"
+      printf "$error\n\n"
+    fi
   elif [ $result -eq $((1 - $expected)) ] # 0 or 1
   then
+    printf '%*.*s' 0 70 "$f $pad"
     echo -e " [\e[31mfailed\e[39m]"
+    printf "$error\n\n"
   else
     echo "There was something wrong with the diff command"
   fi
 done
-
-
