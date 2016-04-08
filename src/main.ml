d18d27b277cda9d7ae8980905ca6ad53dedc8390
Cheuk Chuen Siow <cheukchuen@gmail.com>
2016-04-08 15:44:10 -0400

    Add flags to display version and help

--- src/main.ml
+++ src/main.ml
@@ -3,10 +3,12 @@ open Context
 
 (* utilities *)
 let write f obj name suff = f obj (open_out (name^suff))
+let file = ref ""
 let dumpsymtab = ref false
 let smartsymtab = ref false
 let pptype = ref false
-let file = ref "out.go"
+let version = ref false
+let help = ref false
 
 (* actions *)
 let lex lexbuf =
@@ -84,13 +86,17 @@ let main =
         "Enables a smarter symbol table with lexical based indentation");
      ("-pptype",
         Arg.Set pptype,
-        "Enables pretty print of the program, with the type of each expression printed in some legible format")]
+        "Enables pretty print of the program, with the type of each expression printed in some legible format");
+     ("-v",
+        Arg.Set version,
+        "Print version and exit");
+     ("-h",
+        Arg.Set help,
+        "Display this list of options")]
   in
-  let usage_msg = "<path_to_src>/main.native [lex|parse|weed|pretty|type|compile] [-dumpsymtab] [-pptype] [<path_to_programs>/foo/bar.go]" in
-  (*[-dumpsymtab|-pptype]*)(* add -dumpsymtabll? *)
+  let usage_msg = "Usage: <path_to_src>/main.native [lex|parse|weed|pretty|type|compile] [-dumpsymtab] [-smartsymtab] [-pptype] <path_to_programs>/foo/bar.go" in
   let action = ref compile in
   let in_channel = ref stdin in
-  (*let file = ref "foo/bar.go" in*)
   let anon_fun = function
     | "lex"       -> action := lex
     | "parse"     -> action := parse
@@ -99,7 +105,6 @@ let main =
     | "pretty"    -> action := pretty
 *)
     | "type" -> action := typecheck
-
     | "compile"   -> action := compile
     (* unknown arguments are considered as files *)
     | _ as f    -> (in_channel := open_in f;
@@ -107,17 +112,27 @@ let main =
   in
   begin
     Arg.parse speclist anon_fun usage_msg;
-    let lexbuf = Lexing.from_channel !in_channel in
-    try
-      !action lexbuf
-    with
-      | Error.CompileError message ->
-         Printf.eprintf "Invalid %s\n" message;
-         exit 1
-      | Parser.Error ->
-         Printf.eprintf "Invalid %s"
-           (Error.print_error lexbuf.Lexing.lex_curr_p "syntax error");
-         exit 1
+    if !file <> "" then
+      begin
+        let lexbuf = Lexing.from_channel !in_channel in
+        try
+          !action lexbuf
+        with
+          | Error.CompileError message ->
+             Printf.eprintf "Invalid %s\n" message;
+             exit 1
+          | Parser.Error ->
+             Printf.eprintf "Invalid %s"
+               (Error.print_error lexbuf.Lexing.lex_curr_p "syntax error");
+             exit 1
+      end
+    else
+      begin
+        if !version then
+          print_endline "GoLite to WebAssembly compiler written in OCaml, version 4.0";
+        if !help then
+          Arg.usage speclist usage_msg;
+      end
   end
 
 let _ = main
